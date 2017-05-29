1b20fca94c7a6c5da13d7b8c1ee8237a5dc815f4
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2017-05-29 20:25:10 +0200

    .travis: Install ARM compilers

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,10 +2,12 @@
 # Install a toolchain
 
 if [ "$TOOLCHAIN" = debian ]; then
-	sudo apt-get install gcc-powerpc-linux-gnu
+	sudo apt-get install gcc-powerpc-linux-gnu gcc-arm-none-eabi
 elif [ "$TOOLCHAIN" = devkitppc ]; then
 	wget https://raw.githubusercontent.com/devkitPro/installer/master/perl/devkitPPCupdate.pl
 	perl ./devkitPPCupdate.pl
+	wget https://raw.githubusercontent.com/devkitPro/installer/master/perl/devkitARMupdate.pl
+	perl ./devkitARMupdate.pl
 else
 	echo "Unknown toolchain <$TOOLCHAIN>"
 	exit 1
