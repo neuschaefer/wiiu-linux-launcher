93d1e3ca45279d555d2fad7d853d47180432dd93
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2017-07-12 17:11:23 +0200

    arm: Add types.h (defining u8/u16/u32/...)
    I use stdint.h in my code, but memory.c needs the short u8/u16/u32
    types.

---
+++ arm/types.h
@@ -0,0 +1,23 @@
+/*
+ * Wii U Linux Launcher -- A few typedefs for fixed-size integer types
+ *
+ * Copyright (C) 2017  Jonathan Neuschäfer <j.neuschaefer@gmx.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program, in the file LICENSE.GPLv2.
+ */
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
